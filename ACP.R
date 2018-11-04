8cbbe187e0630b1a0eff1eb5ee75b6dda457a8a8
Hugo <lebaherhugo@gmail.com>
2018-11-04 16:30:57 +0100

    analyse var/individus

--- ACP.R
+++ ACP.R
@@ -1,4 +1,4 @@
-acpn = function(data_in){
+acpn = function(data_in, round_precision = 3){
   
   result = list()
   #Filtre
@@ -37,12 +37,51 @@ acpn = function(data_in){
   #Composantes principales
   result$comp = result$cenred %*% result$eigen$vectors
   
-  #Inertie
-  result$inertia <- apply(result$comp^2,1,sum)/num_col 
+  #Rapport de l'inertie
+  result$inertiaratio = acp_eau$eigen$values / num_col
+  
+  #Inertie cumulée
+  result$inertiasum = round(sum(result$inertiaratio),3)*100
+
+  #Contribution relative des individus
+  result$qlti = (result$comp)^2
+  for (i in 1:nrow(result$qlti)) {
+    for (k in 1:ncol(result$qlti)) {
+      result$qlti[i,k] = result$qlti[i,k] / (sum(result$comp[i,]^2))
+    }
+  }
+  result$qtli = round(result$qlti, round_precision)
+  
+  #Contribution absolue des individus
+  result$ctri = (result$comp)^2
+  for (i in 1:nrow(result$ctri)) {
+    for (k in 1:ncol(result$ctri)) {
+      result$ctri[i,k] = result$ctri[i,k] / (ncol(result$ctri)*result$eigen$values[k])
+    }
+  }
+  result$ctri = round(result$ctri, round_precision)
   
   #Matrice des coordonnées de variables
-  result$stddev = diag(sapply(diag(as.matrix(result$eigen$values)), function(x){1/sqrt(x)}))
-  #result$coords = 
+  D = diag(sapply(as.matrix(result$eigen$values), sqrt))
+  result$coords = result$eigen$vectors %*% D
+  
+  #Contribution relative des variables
+  result$qltv = (result$coords)^2
+  for (j in 1:nrow(result$qltv)) {
+    for (k in 1:ncol(result$qltv)) {
+      result$qltv[j,k] = result$qltv[j,k] / (sum(result$coords[j,]^2))
+    }
+  }
+  result$qtlv = round(result$qltv, round_precision)
+  
+  #Contribution absolue des variables
+  result$ctri = (result$comp)^2
+  for (i in 1:nrow(result$ctri)) {
+    for (k in 1:ncol(result$ctri)) {
+      result$ctri[i,k] = result$ctri[i,k] / (ncol(result$ctri)*result$eigen$values[k])
+    }
+  }
+  result$ctri = round(result$ctri, round_precision)
   
   #Retour
   return(result)
