1480690f20d224c0f3e9e8f73ceea565c8560035
Hugo <lebaherhugo@gmail.com>
2018-11-18 22:49:41 +0100

    modif

--- ACP.R
+++ ACP.R
@@ -135,5 +135,8 @@ legend(x = "bottomright",legend=unique(water_type),col=1:length(water_type),pch=
 #eaux_actives <- filter(eaux, Pays== "France")
 dudi = dudi.pca(df = acp_waters$raw_data, scannf = FALSE, nf = 3)
 
-cor(acp_waters$matrix[setdiff(c(1:nrow(acp_waters$matrix)), unique(unlist(apply(acp_waters$matrix, 2, function(x){which(x %in% boxplot.stats(x)$out)})))),])
+nrow(acp_waters$matrix)
+num_outliers = unique(unlist(apply(acp_waters$matrix, 2, function(x){which(x %in% boxplot.stats(x)$out)})))
+with_outliers = acp_waters$matrix[num_outliers,]
+without_outliers = acp_waters$matrix[setdiff(c(1:nrow(acp_waters$matrix)), num_outliers),]
 
